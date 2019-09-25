c9bccc8b34761e71fc8183459b46bc0a65b816d2
willyowi <wilsonowino1@gmail.com>
2019-09-25 16:42:58 +0300

    changed the category   r.ship

--- universal_billing_system/models.py
+++ universal_billing_system/models.py
@@ -5,14 +5,15 @@ class Industry(models.Model):
     name = models.CharField( blank=False,max_length= 40,default='JamboPay')
     def __str__(self):
         return self.name 
-class Revstreams(models.Model):
-    name = models.CharField( blank=False,max_length= 40,default='JamboPay')
-    def __str__(self):
-        return self.name
 class Category(models.Model):
     name = models.CharField( blank=False,max_length= 40,default='JamboPay')
     def __str__(self):
         return self.name                    
+class Revstreams(models.Model):
+    name = models.CharField( blank=False,max_length= 40,default='JamboPay')
+    Category = models.ManyToManyField(Category)
+    def __str__(self):
+        return self.name
 class Merchant(models.Model):
     Business_name = models.CharField(max_length=20,blank=False)
     Email = models.EmailField()
@@ -23,7 +24,7 @@ class Merchant(models.Model):
     JP_paybill = models.CharField(max_length=20,blank=False)
     Industry = models.ManyToManyField(Industry)
     Revstreams = models.ManyToManyField(Revstreams)
-    Category = models.ManyToManyField(Category)
+    
     
 
 
