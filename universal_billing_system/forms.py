586813c578fe130d100f31ada48b5ed93d2221e3
Pixel-0 <tiny0pixel@gmail.com>
2019-09-25 15:18:30 +0300

    add login form

---
+++ universal_billing_system/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+
+class LoginForm(AuthenticationForm):
+   '''
+   User login form.
+   '''
+   
+   username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+   password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+
+   class Meta:
+      model = User
+      fields = ['username','password']
