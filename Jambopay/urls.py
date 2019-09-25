db60bf5fe6f9ee3e3c0630b38be166873b483c2b
Pixel-0 <tiny0pixel@gmail.com>
2019-09-25 17:21:56 +0300

    render index template

--- Jambopay/urls.py
+++ Jambopay/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('universal_billing_system.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
--- universal_billing_system/views.py
+++ universal_billing_system/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
 def index(request):
     
     return render(request,'index.html')
+
+@login_required(login_url='register')
+def logout_view(request):
+   logout(request)
+   return redirect('login')
