21ac0d5d0ef0f88d0781529557bc296844b01ac9
Johnonyango <j.yayah7@gmail.com>
2019-09-26 22:40:56 +0300

    create view function for the api for merchant details

--- universal_billing_system/views.py
+++ universal_billing_system/views.py
@@ -15,50 +15,20 @@ from . models import Merchant
 
 # Create your views here.
 def index(request):
-    url = ('jpaye.herokuap.com/api/GetMerchants/')
+    url = ('https://jpaye.herokuapp.com/api/GetMerchants')
     response = requests.get(url)
-    print(response)
-   
-def index(request):
-    return render(request, 'index.html')
-        # url = 'http://127.0.0.1:8080/api/Merchants/'
-        # r = requests.get(url.format()).json()
-        # mechant = r['all_merchants']
-        # print(r.all_merchants)
-        # merchant_details = []
-        # for merchant_item in merchant:
-        #     id = merchant_item.get('id')
-        #     name = merchant_item.get('Business_name')
-        #     email = merchant_item.get('email')
-        #     Phone_number = merchant_item.get('Phone_number')
-        #     address = merchant_item.get('Physical_address')
-        #     code = merchant_item.get('Post_code'),
-        #     Town = merchant_item.get('Town')
-        #     paybill = merchant_item.get('JP_paybill')
-        #     Industry = merchant_item.get('Industry')
+    details = response.json()
+    for detail in details:
+        Business_name = detail.get('Business_name')
+        Email = detail.get('Email')
+        Phone_number = detail.get('Phone_number')
+        Address = detail.get('Physical_address')
+        Code = detail.get('Post_code')
+        Town = detail.get('Town')
+        Pay_bill = detail.get('JP_paybill')
+        Industry = detail.get('Industry')
+    return render(request, 'index.html', {'details': details})
 
-
-        #     if name:
-        #         merchant_object = Merchant(id,name,email,Phone_number,address,code,Town, paybill,Industry)
-        #         merchant_details.append(merchant_object)
-                
-        # return render(request, "index.html", {"details": merchant_details})
-        # url = 'http://127.0.0.1:8000/api/Merchants/'
-        # Business_name = 'Vinka'
-        # r = requests.get(url.format(Business_name)).json()
-        # merchant_details = {
-        # "Business_name":'r.main', 
-        # "Email": '',
-        # "Phone_number":'',
-        # "Physical_address":'',
-        # "Post_code":'',
-        # "Town":'',
-        # "JP_paybill":'',
-        # "Industry":'',
-        # } 
-        # context = {
-        #     'merchant' :merchant}
-        # return render(request,'index.html')
 
 def bills(request):
     return render(request, 'bills.html')
