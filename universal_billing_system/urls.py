8202cd27df4bb379cfe11aac682b8204fadd7ebd
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-09-26 15:47:41 +0300

    deploying again

--- universal_billing_system/urls.py
+++ universal_billing_system/urls.py
@@ -2,8 +2,6 @@ from django.conf.urls import url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-from allauth.account.views import ConfirmEmailView
-
 urlpatterns=[
     url(r'^$',views.index,name='Index'),
 
