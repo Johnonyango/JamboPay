7eb1f993f98be2396f96874b5ad5cfc134fe074f
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-09-26 15:34:29 +0300

    deploying again

--- Jambopay/settings.py
+++ Jambopay/settings.py
@@ -111,7 +111,6 @@ WSGI_APPLICATION = 'Jambopay.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-<<<<<<< HEAD
 if config('MODE')=="dev":
    DATABASES = {
        'default': {
@@ -134,9 +133,6 @@ else:
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-=======
-
->>>>>>> mannu-rest-api-merchants
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
