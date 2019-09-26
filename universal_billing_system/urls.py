34015a668d6520302c5518d4fad59806953f8698
Johnonyango <j.yayah7@gmail.com>
2019-09-26 22:37:12 +0300

    create url path for merchant detail

--- universal_billing_system/urls.py
+++ universal_billing_system/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url(r'^$',views.index,name='Index'),
-
+    
     #API Endpoints
     url(r'^api/GetMerchants/$', views.MerchantList.as_view()),
     url(r'^api/GetRevenueStreams/$', views.RevenueStreamsList.as_view()),
