c04bb372960b10e6ee41be78640f296b93858fb3
willyowi <wilsonowino1@gmail.com>
2019-09-27 18:14:37 +0300

    trying to upload a file

--- universal_billing_system/views.py
+++ universal_billing_system/views.py
@@ -6,6 +6,11 @@ from .serializer import *
 from .permissions import IsAdminOrReadOnly
 from rest_framework import status
 from . models import Merchant
+import openpyxl
+# import pyexcel-ods
+# import pyexcel-xls
+# import pyexcel-xlsx
+
 # from .forms import *
 
 
@@ -25,51 +30,46 @@ def login(request):
 
 
 # Create your views here.
-def index(request):
-    url = ('jpaye.herokuap.com/api/GetMerchants/')
-    response = requests.get(url)
-    print(response)
+# def index(request):
+#     url = ('jpaye.herokuap.com/api/GetMerchants/')
+#     response = requests.get(url)
+#     print(response)
    
+# def index(request):
+#     return render(request, 'index.html')
+
 def index(request):
-    return render(request, 'index.html')
-        # url = 'http://127.0.0.1:8080/api/Merchants/'
-        # r = requests.get(url.format()).json()
-        # mechant = r['all_merchants']
-        # print(r.all_merchants)
-        # merchant_details = []
-        # for merchant_item in merchant:
-        #     id = merchant_item.get('id')
-        #     name = merchant_item.get('Business_name')
-        #     email = merchant_item.get('email')
-        #     Phone_number = merchant_item.get('Phone_number')
-        #     address = merchant_item.get('Physical_address')
-        #     code = merchant_item.get('Post_code'),
-        #     Town = merchant_item.get('Town')
-        #     paybill = merchant_item.get('JP_paybill')
-        #     Industry = merchant_item.get('Industry')
+    if "GET" == request.method:
+        return render(request, 'index.html', {})
+    else:
+        excel_file = request.FILES["excel_file"]
 
+        # you may put validations here to check extension or file size
 
-        #     if name:
-        #         merchant_object = Merchant(id,name,email,Phone_number,address,code,Town, paybill,Industry)
-        #         merchant_details.append(merchant_object)
-                
-        # return render(request, "index.html", {"details": merchant_details})
-        # url = 'http://127.0.0.1:8000/api/Merchants/'
-        # Business_name = 'Vinka'
-        # r = requests.get(url.format(Business_name)).json()
-        # merchant_details = {
-        # "Business_name":'r.main', 
-        # "Email": '',
-        # "Phone_number":'',
-        # "Physical_address":'',
-        # "Post_code":'',
-        # "Town":'',
-        # "JP_paybill":'',
-        # "Industry":'',
-        # } 
-        # context = {
-        #     'merchant' :merchant}
-        # return render(request,'index.html')
+        wb = openpyxl.load_workbook(excel_file)
+
+        # getting a particular sheet by name out of many sheets
+        worksheet = wb["Sheet1"]
+        print(worksheet)
+
+        excel_data = list()
+        # iterating over the rows and
+        # getting value from each cell in row
+        for row in worksheet.iter_rows():
+            row_data = list()
+            for cell in row:
+                row_data.append(str(cell.value))
+            excel_data.append(row_data)
+
+            #` getting all sheets
+            # sheets = wb.sheetnames
+            # print(sheets)
+            # getting active sheet
+            # active_sheet = wb.active
+            # print(active_sheet)`
+
+        return render(request, 'index.html', {"excel_data":excel_data})
+       
 
 def bills(request):
     return render(request, 'bills.html')
