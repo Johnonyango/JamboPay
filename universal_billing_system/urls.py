912957721bb5fcc87c64022edd47ae8f679a4009
Johnonyango <j.yayah7@gmail.com>
2019-09-27 17:10:12 +0300

    solving conflicts from manu

--- universal_billing_system/urls.py
+++ universal_billing_system/urls.py
@@ -5,14 +5,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns=[
-<<<<<<< HEAD
-    # url(r'^$',views.index,name='home'),
-    url(r'^merchants/$',views.merchants,name='merchants'),
-    #API Endpoints
-    url(r'^api/GetMerchants/$', views.MerchantList.as_view()),
-    url(r'^api/GetRevenueStreams/$', views.RevenueStreamsList.as_view()),
-] 
-=======
     url(r'^$',views.index,name='Index'),
     url(r'^merchants/$',views.merchants,name='merchants'),
 
@@ -27,7 +19,6 @@ urlpatterns=[
     url(r'^index/$', views.index, name='home'),
     url(r'^bills/$', views.bills, name='bills'),
 ]
->>>>>>> manu
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
