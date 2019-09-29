3accb02fc27a92df0eab6bdc6141f2ce54275b43
willyowi <wilsonowino1@gmail.com>
2019-09-29 19:51:07 +0300

    view fuction for upload made

--- universal_billing_system/views.py
+++ universal_billing_system/views.py
@@ -105,3 +105,42 @@ def merchants(request):
         Pay_bill = detail.get('JP_paybill')
         Industry = detail.get('Industry')
     return render(request, 'merchants.html', {'details': details})
+
+
+
+def upload(request):
+    if "GET" == request.method:
+        return render(request, 'upload.html', {})
+    else:
+        excel_file = request.FILES["excel_file"]
+
+        # you may put validations here to check extension or file size
+
+        wb = openpyxl.load_workbook(excel_file)
+
+        # getting all sheets
+        sheets = wb.sheetnames
+        # print(sheets)
+
+        # getting a particular sheet
+        worksheet = wb["Sheet1"]
+        # print(worksheet)
+
+        # getting active sheet
+        active_sheet = wb.active
+        # print(active_sheet)
+
+        # reading a cell
+        print(worksheet["A1"].value)
+
+        excel_data = list()
+        # iterating over the rows and
+        # getting value from each cell in row
+        for row in worksheet.iter_rows():
+            row_data = list()
+            for cell in row:
+                row_data.append(str(cell.value))
+                print(cell.value)
+            excel_data.append(row_data)
+
+        return render(request, 'upload.html', {"excel_data":excel_data})  
