d6e55d7fdabd7a086dfdeb72e018a66e35c916d1
Johnonyango <j.yayah7@gmail.com>
2019-09-30 09:13:47 +0300

    update templates and views

--- universal_billing_system/views.py
+++ universal_billing_system/views.py
@@ -8,7 +8,7 @@ from .permissions import IsAdminOrReadOnly
 from rest_framework import status
 from . models import Merchant
 import requests
-# from .forms import *
+from .forms import *
 
 
 # login
