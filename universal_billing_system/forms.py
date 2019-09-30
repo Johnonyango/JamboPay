e0f48dc4d467ac282dec8c1a362b2c3b8d3716c3
Pixel-0 <tiny0pixel@gmail.com>
2019-09-30 09:55:58 +0300

    accept changes from another branch

--- universal_billing_system/forms.py
+++ universal_billing_system/forms.py
@@ -1,21 +1,12 @@
-from django import forms	
+from django import forms    
 from .models import *
-# from django.contrib.auth.models import User	
-# # from django.contrib.auth.forms import MerchanLoginForm
-# from django.contrib.auth.forms import MerchantLoginForm
-# from django.contrib.auth.models import Merchant
-
-# # sign up forms
-# class LoginForm(MerchantLoginForm):
-#    	email = forms.EmailField(max_length=200, help_text='Required')
-   	   
-#    class Meta:
-#    	        model = Merchant
+from django.contrib.auth.models import User    
+# from django.contrib.auth.forms import MerchanLoginForm
+from django.contrib.auth.forms import MerchantLoginForm
+from django.contrib.auth.models import Merchant
+# sign up forms
+class LoginForm(MerchantLoginForm):
+      email = forms.EmailField(max_length=200, help_text='Required')
          
-
-class GenerateBillForm(forms.ModelForm):
-    class Meta:
-       models = Bills
-       fields ='__all__'
-
-         
+      class Meta:
+              model = Merchant
