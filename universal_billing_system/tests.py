2d2c689c1eed1790888d51925be10d7611e20964
Johnonyango <j.yayah7@gmail.com>
2019-10-03 13:35:02 +0300

    write more tests for bills, revstream and merchant classes

--- universal_billing_system/tests.py
+++ universal_billing_system/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from .models import *
+from django.contrib.auth.models import User
+import datetime as dt
 
 class IndustryTestClass(TestCase):
     def setUp(self):
@@ -12,3 +14,27 @@ class IndustryTestClass(TestCase):
         self.hospitality.save_industry()
         industries = Industry.objects.all()
         self.assertTrue(len(industries))
+
+class MerchantTestClass(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='Emmanuel')
+        # self.revstreams = Revstreams.objects.create_revstream(revstream = 'Food & Beverages')
+        # self.industry = Industry.objects.create_industry(industry = 'Ho')
+        self.aquamist = Merchant(Business_name = 'Aquamist', Business_owner = self.user,Email = 'john@gmail.com', Phone_number = '80745637', Physical_address ='P.O Box 10702', Post_code = '00100', Town = 'Nairobi', JP_paybill = '23451', Industry = 'Hospitality', Revstreams ='Food & Beverages', join_date = '2019-09-01' )
+        
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.aquamist,Merchant))
+
+
+# class BillsTestCase(TestCase):
+#     def setUp(self):
+#         self.james= Bills(customer_name = 'James', customer_phone ='9087645', customer_email ='james@moringaschool.com', Revstreams ='Food & Beverages', narration ='deliverd on October', amount ='20000', quantity ='30', post_date ='2019-09-01',status ='unpaid')
+    
+#     def test_instance(self):
+#         self.assertTrue(isinstance(self.james,Bills))
+
+# class PaymentsTestCase(TestCase):
+#     def setUp(self):
+#         self.james= Payments(bill_number ='9087645',payers_name = 'James',  customer_email ='james@moringaschool.com', Revstreams ='Food & Beverages', narration ='deliverd on October', amount ='20000', quantity ='30', post_date ='2019-09-01',status ='unpaid')
+
