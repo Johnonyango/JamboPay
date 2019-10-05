8f87ff4061faff45b2b8ba787fc4c0968627074c
willyowi <wilsonowino1@gmail.com>
2019-10-05 15:15:26 +0300

    return the val generated

--- universal_billing_system/bill_num.py
+++ universal_billing_system/bill_num.py
@@ -4,6 +4,6 @@ import string
 def randomStringDigits(stringLength=8):
     """Generate a random string of letters and digits """
   
-#     lettersAndDigits = string.ascii_letters + string.digits
-#     val=''.join(random.choice(lettersAndDigits) for i in range(stringLength))
-#     return val
+    lettersAndDigits = string.ascii_letters + string.digits
+    val=''.join(random.choice(lettersAndDigits) for i in range(stringLength))
+    return val
