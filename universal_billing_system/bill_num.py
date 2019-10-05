d51f36048bad8681d6257087c5377bd5c191b923
willyowi <wilsonowino1@gmail.com>
2019-10-05 15:13:53 +0300

    importing string and random

---
+++ universal_billing_system/bill_num.py
@@ -0,0 +1,9 @@
+import random
+import string
+​
+# def randomStringDigits(stringLength=8):
+#     """Generate a random string of letters and digits """
+  
+#     lettersAndDigits = string.ascii_letters + string.digits
+#     val=''.join(random.choice(lettersAndDigits) for i in range(stringLength))
+#     return val
