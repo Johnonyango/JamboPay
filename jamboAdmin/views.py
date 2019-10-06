d3e6f3176940977726e960ce0f2b56675e8abac5
Johnonyango <j.yayah7@gmail.com>
2019-10-06 21:37:31 +0300

    add sign function in the admin views

--- jamboAdmin/views.py
+++ jamboAdmin/views.py
@@ -42,3 +42,29 @@ def merchants(request):
         Pay_bill = detail.get('JP_paybill')
         Industry = detail.get('Industry')
     return render(request, 'merchants.html', {'details': details})
+
+
+def sign(request):
+    current_user = request.user
+    if request.method == "POST":
+        form = BillsForm(request.POST, request.FILES)
+        if form.is_valid():
+            bill = form.save(commit=False)
+            bill.save()
+
+        # if request.method=="POST":
+        # form =BillsForm(request.POST)
+        # if form.is_valid():
+            name = form.cleaned_data.get('customer_name')
+            email = form.cleaned_data.get('customer_email')
+            amount = form.cleaned_data.get('amount')
+            quantity = form.cleaned_data.get('quantity')
+
+        #     name = request.POST.get('customer_name')
+        #     email = request.POST.get('customer_email')
+            recipient = NewsLetterRecipients(name=name, email=email,amount=amount,quantity=quantity)
+            recipient.save()
+            send_notification(name, email,amount=amount,quantity=quantity)
+            # recipient = NewsLetterRecipients(name = name,email =email)
+            # send_notification(name = name, email = email)
+
