faf8520ac20645cd3775f95150e15c77acc21b31
Johnonyango <j.yayah7@gmail.com>
2019-10-06 21:55:54 +0300

    create email.py file in the Jambo admin app

--- universal_billing_system/email.py
+++ universal_billing_system/email.py
@@ -13,14 +13,4 @@ def send_notification(name,email,amount,quantity):
     msg.attach_alternative(html_content,'text/html')
     msg.send()
 
-def send_message(Business_owner, email):
-    subject = 'account created'
-    sender = 'j.yayah7@gmail.com'
-
-    text_content = render_to_string('email/bill.txt',{ "Business_owner": Business_owner })
-    html_content = render_to_string('email/bill.html',{ "Business_owner": Business_owner })
-
-    msg = EmailMultiAlternatives(subject,text_content,sender,[email])
-    msg.attach_alternative(html_content,'text/html')
-    msg.send()
 
